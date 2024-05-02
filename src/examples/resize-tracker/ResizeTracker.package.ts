ee248b2a0de0feee8b22035492c98d85908d8e07
TeslaAdis <adis.talic@bitmovin.com>
2024-05-02 09:47:09 +0200

    Simplify resize tracker package

--- src/examples/resize-tracker/ResizeTracker.package.ts
+++ src/examples/resize-tracker/ResizeTracker.package.ts
@@ -1,10 +1,13 @@
+import { Abortable } from '@bitmovin/player-web-x/framework-types/abortable/Abortable';
 import { EmptyObject } from '@bitmovin/player-web-x/framework-types/BaseTypes';
 import type { ContextHaving, ContextUsing } from '@bitmovin/player-web-x/framework-types/execution-context/Types';
 import { createPackage, createTask } from '@bitmovin/player-web-x/playerx-framework-utils';
 import type { CoreEffects } from '@bitmovin/player-web-x/types/framework/core/core/Core.package';
 import type { StoreEffectFactory } from '@bitmovin/player-web-x/types/framework/core/core/state/StoreEffectFactory';
+import type { StateAtom } from '@bitmovin/player-web-x/types/framework/core/core/state/Types';
 import type { Logger } from '@bitmovin/player-web-x/types/framework/core/core/utils/Logger';
 import type { SourceStateAtom } from '@bitmovin/player-web-x/types/framework/core/source/atoms/SourceStateAtom';
+import { VideoElementAtom } from '@bitmovin/player-web-x/types/framework/core/source/atoms/VideoElementAtom';
 import type { ContextWithState } from '@bitmovin/player-web-x/types/framework/core/Types';
 import type { ComponentName } from '@bitmovin/player-web-x/types/framework/Types';
 
@@ -12,8 +15,6 @@ import type { ResizeObserverEffect } from './ResizeObserverEffectFactory';
 import { ResizeObserverEffectFactory } from './ResizeObserverEffectFactory';
 import type { ResizeTrackerStateAtom } from './ResizeTrackerStateAtom';
 import { createResizeTrackerStateAtom } from './ResizeTrackerStateAtom';
-import type { VideoElementStateAtom } from './VideoElementStateAtom';
-import { createVideoElementStateAtom } from './VideoElementStateAtom';
 
 type Dependencies = {
   [ComponentName.CoreEffects]: CoreEffects;
@@ -34,7 +35,6 @@ export type VideoStateContext = ContextHaving<
   ResizeTrackerExports,
   ContextUsing<
     [
-      StoreEffectFactory<'videoElementState', VideoElementStateAtom>,
       StoreEffectFactory<'resizeTrackerState', ResizeTrackerStateAtom>,
       ResizeObserverEffect,
     ],
@@ -49,11 +49,9 @@ export const ResizeTrackerPackage = createPackage<Dependencies, ResizeTrackerExp
       baseContext.registry.get('core-effects');
 
     const contextWithState = baseContext.using(StateEffectFactory).using(EventListenerEffectFactory);
-    const videoElementState = createVideoElementStateAtom(contextWithState);
     const resizeTrackerState = createResizeTrackerStateAtom(contextWithState);
 
     const contextWithVideoState = contextWithState
-      .using(StoreEffectFactory('videoElementState', videoElementState))
       .using(StoreEffectFactory('resizeTrackerState', resizeTrackerState))
       .using(ResizeObserverEffectFactory);
 
@@ -62,15 +60,19 @@ export const ResizeTrackerPackage = createPackage<Dependencies, ResizeTrackerExp
     const sourceState = baseContext.registry.get('source-state');
     const { state } = contextWithVideoState.effects;
 
-    state.subscribe(contextWithVideoState, videoElementState, VideoElementSubscriber);
-    state.subscribe(contextWithVideoState, sourceState, SourceStateChangeSubscriber);
+    const initial = contextWithVideoState.fork(VideoElementSubscriber(), sourceState.video, () => true);
+    initial.catch(() => {/* */})
+    state.subscribe(contextWithVideoState, sourceState.video, VideoElementSubscriber(initial));
   },
   ['core-effects', 'source-state', 'logger'],
 );
 
-const VideoElementSubscriber = createTask(
+const VideoElementSubscriber = (initialAbortable?: Abortable) => createTask(
   'video-element-subscriber',
-  (videoElementState: VideoElementStateAtom, context: VideoStateContext) => {
+  (videoElementState: VideoElementAtom, context: VideoStateContext) => {
+    if (initialAbortable) {
+      initialAbortable.abort(new Error('Aborted due to subscriber triggering'))
+    }
     const video = videoElementState.element;
 
     if (video === undefined) {
@@ -86,18 +88,6 @@ const VideoElementSubscriber = createTask(
       logger.log('[RT]', entry);
       state.dispatch(resizeTrackerState.set, entry);
     });
-
-    return context.effects.loop(context.abortSignal);
-  },
-);
-
-const SourceStateChangeSubscriber = createTask(
-  'source-state-change-subscriber',
-  (source: SourceStateAtom, context: VideoStateContext) => {
-    const { state, store } = context.effects;
-    const { videoElementState } = store;
-
-    state.dispatch(videoElementState.set, source.video.element);
 
     return context.effects.loop(context.abortSignal);
   },
