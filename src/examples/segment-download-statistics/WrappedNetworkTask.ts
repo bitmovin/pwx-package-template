ed688923e069f2880b9ccee1254b99545f6a0e06
Wolfram Hofmeister <wolfram.hofmeister@bitmovin.com>
2024-05-08 18:13:45 +0200

    Prepare sample for new player version

--- src/examples/segment-download-statistics/WrappedNetworkTask.ts
+++ src/examples/segment-download-statistics/WrappedNetworkTask.ts
@@ -1,41 +1,30 @@
-import { createTask } from '@bitmovin/player-web-x/playerx-framework-utils';
+import { createTaskClosure } from '@bitmovin/player-web-x/playerx-framework-utils';
 import type { NetworkAtom } from '@bitmovin/player-web-x/types/framework/core/core/network/NetworkAtom';
-import type { NetworkTask } from '@bitmovin/player-web-x/types/framework/core/network/NetworkTask';
 import type { BaseRequestConfig, HttpResponseType } from '@bitmovin/player-web-x/types/framework/core/network/Types';
 
 import { CustomComponentName } from './CustomComponents';
 import type { NetworkInspectorContext } from './NetworkInspector.package';
-
-const WrapperIdentifierSymbol = Symbol();
-
-export function hasWrappedTask(context: NetworkInspectorContext) {
-  const networkTask = context.registry.get('network-task');
-
-  return WrapperIdentifierSymbol in networkTask;
-}
+import { NetworkPackageContext } from '@bitmovin/player-web-x/types/framework/core/network/Network.package';
+import { NetworkTask } from '@bitmovin/player-web-x/types/framework/core/network/NetworkTask';
 
 export const wrapNetworkTask = (context: NetworkInspectorContext): void => {
-  const networkTask = context.registry.get('network-task');
   const downloadInfoAtom = context.registry.get(CustomComponentName.DownloadInfoAtom);
 
   if (downloadInfoAtom === undefined) {
     throw new Error('DownloadInfoAtom unexpectedly undefined');
   }
 
-  const wrappedTask = createTask(
+  const WrappedNetworkTaskClosure = createTaskClosure((originalNetworkTask: typeof NetworkTask) => [
     'network-task-wrapper',
-    async (data: [BaseRequestConfig<HttpResponseType>, NetworkAtom], context: NetworkInspectorContext) => {
+    async (data: [BaseRequestConfig<HttpResponseType>, NetworkAtom | undefined], context: NetworkPackageContext) => {
       const state = context.effects.state;
-      const response = await context.fork(networkTask, data);
+      const response = await context.fork(originalNetworkTask, data);
 
       state.dispatch(downloadInfoAtom.trackDownload, response);
 
       return response;
     },
-  ) as unknown as typeof NetworkTask;
+  ]);
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (wrappedTask as any)[WrapperIdentifierSymbol] = 73;
-
-  context.registry.set('network-task', wrappedTask);
+  context.registry.addReplacer('network-task', WrappedNetworkTaskClosure);
 };
